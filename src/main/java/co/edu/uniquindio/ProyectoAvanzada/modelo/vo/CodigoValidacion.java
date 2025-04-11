2723c610b5761fb768a1dfe31630dee602f30e35
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2025-04-10 21:13:20 -0500

    codigo para validar usuarios

--- src/main/java/co/edu/uniquindio/ProyectoAvanzada/modelo/vo/CodigoValidacion.java
+++ src/main/java/co/edu/uniquindio/ProyectoAvanzada/modelo/vo/CodigoValidacion.java
@@ -1,8 +1,18 @@
 package co.edu.uniquindio.ProyectoAvanzada.modelo.vo;
 
-import java.time.LocalDate;
+import lombok.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CodigoValidacion {
     private String codigo;
-    private LocalDate fecha;
+    private LocalDateTime fecha;
 }

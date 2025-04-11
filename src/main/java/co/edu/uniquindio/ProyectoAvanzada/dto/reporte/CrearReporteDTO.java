0af969271f0773bc831fb370600b666841784391
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2025-04-10 21:12:35 -0500

    actualizacion de DTOs claves

--- src/main/java/co/edu/uniquindio/ProyectoAvanzada/dto/reporte/CrearReporteDTO.java
+++ src/main/java/co/edu/uniquindio/ProyectoAvanzada/dto/reporte/CrearReporteDTO.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.ProyectoAvanzada.dto.reporte;
 
 import co.edu.uniquindio.ProyectoAvanzada.modelo.documentos.Categoria;
+import co.edu.uniquindio.ProyectoAvanzada.modelo.enums.EstadoReporte;
 import co.edu.uniquindio.ProyectoAvanzada.modelo.vo.Ubicacion;
 
 import java.util.List;
@@ -10,6 +11,7 @@ public record CrearReporteDTO(
         Categoria categoria,
         String descripcion,
         Ubicacion ubicacion,
+        EstadoReporte estado,
         List<String> imagenes
 ) {
 }
--- src/main/java/co/edu/uniquindio/ProyectoAvanzada/dto/usuario/EditarUsuarioDTO.java
+++ src/main/java/co/edu/uniquindio/ProyectoAvanzada/dto/usuario/EditarUsuarioDTO.java
@@ -1,9 +1,6 @@
 package co.edu.uniquindio.ProyectoAvanzada.dto.usuario;
 
-import java.util.List;
-
 public record EditarUsuarioDTO(
-        String id,
         String nombre,
         String email,
         String numTelefono,

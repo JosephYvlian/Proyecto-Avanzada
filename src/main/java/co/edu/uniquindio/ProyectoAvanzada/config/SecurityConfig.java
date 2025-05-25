6eeb74824ce3fdb39e409e6b14b995ad5e72da51
FedeAlvarezzz <federico.alvarezm@uqvirtual.edu.co>
2025-05-24 22:57:42 -0500

    despliegue v3

--- src/main/java/co/edu/uniquindio/ProyectoAvanzada/config/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/ProyectoAvanzada/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         // Configura las políticas de CORS para permitir solicitudes desde el frontend
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("https://nex-front-98e37.web.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
